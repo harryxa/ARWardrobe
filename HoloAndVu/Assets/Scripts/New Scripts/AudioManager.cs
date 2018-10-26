b077ed4e6f82a5403457ad0b17bb1f9971eb7eef
harryxa <harry.alcott@hotmail.co.uk>
2018-10-26 11:57:15 +0100

    hologram placements

---
+++ HoloAndVu/Assets/Scripts/New Scripts/AudioManager.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    //list of all audio for selected item
+    public List<AudioClip> dressAudio = new List<AudioClip>();
+    public List<AudioClip> lipstickAudio = new List<AudioClip>();
+    public List<AudioClip> shoeAudio = new List<AudioClip>();
+    public List<AudioClip> selectedAudio = new List<AudioClip>();
+
+    //list of hotspots in the scene, when hotspoty inits it adds to list
+    public List<GameObject> hotSpots = new List<GameObject>();
+
+    //int to indicate which audio from list to play
+    int audioNumber;
+
+    public enum ItemChoice
+    {
+        LIPSTICK,
+        DRESS,
+        SHOE
+    }
+
+    public ItemChoice itemChoice;
+
+	void Start ()
+    {
+        //item equals dress whilst no choosing method implimented
+        itemChoice = ItemChoice.DRESS;
+        audioNumber = 0;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        ItemSelect();
+    }
+
+    //selects appropriate audio for item selected
+    private void ItemSelect()
+    {
+        switch (itemChoice)
+        {
+            case ItemChoice.DRESS:
+                if (selectedAudio != dressAudio)
+                    selectedAudio = dressAudio;
+                break;
+
+            case ItemChoice.LIPSTICK:
+                if (selectedAudio != lipstickAudio)
+                    selectedAudio = lipstickAudio;
+                break;
+
+            case ItemChoice.SHOE:
+                if (selectedAudio != shoeAudio)
+                    selectedAudio = shoeAudio;
+                break;
+        }
+    }
+
+    public AudioClip GetSelectedAudio()
+    {     
+        return selectedAudio[audioNumber];
+    }
+
+    public void IncrementAudioNumber()
+    {
+        audioNumber++;
+    }
+
+    public void AddToHotSpotList(GameObject hotspot)
+    {
+        hotSpots.Add(hotspot);
+    }
+}
---
+++ HoloAndVu/Assets/Scripts/New Scripts/PlayAudio.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAudio : MonoBehaviour
+{
+    private AudioSource m_AudioSource;
+
+    private GameObject GameObjAudioManager;
+    private AudioManager m_AudioManager;
+
+    private bool soundPlayed = false;
+    private bool incrementAudio = true;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        m_AudioSource = GetComponentInChildren<AudioSource>();
+
+        GameObjAudioManager = GameObject.FindGameObjectWithTag("AudioManager");
+        m_AudioManager = GameObjAudioManager.GetComponent<AudioManager>();
+        m_AudioManager.AddToHotSpotList(this.gameObject);
+        //gameObject.GetComponentInChildren<Transform>().localScale = new Vector3(2, 2, 2);
+    }
+
+    //When entering the trigger appropriate audio is selected and played.
+    //Audio wont replay if entered again as SoundPlayed is true.
+    private void OnTriggerEnter(Collider other)
+    {    
+        if (other.tag == "MainCamera" && soundPlayed == false)
+        {
+            if (m_AudioManager.itemChoice == AudioManager.ItemChoice.DRESS)
+            {
+                m_AudioSource.clip = m_AudioManager.GetSelectedAudio();
+            }
+
+            soundPlayed = true;
+            m_AudioSource.Play();
+            
+        }
+    }
+
+    //When exiting audio[] will be incremented once so next hotspot has correct Audio
+    private void OnTriggerExit (Collider other)
+    {
+        if (other.tag == "MainCamera" && soundPlayed == true)
+        {
+            if(incrementAudio == true)
+            {
+                m_AudioManager.IncrementAudioNumber();
+                incrementAudio = false;
+            }
+        }
+    }
+
+
+}
